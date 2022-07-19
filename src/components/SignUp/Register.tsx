3558bdcae8ff301f7f367be9670287cff707eb14
Jerry Frederic <jfpl.dev@gmail.com>
2022-07-19 13:36:29 +0200

    feat(app): setup firebase

--- src/components/SignUp/Register.tsx
+++ src/components/SignUp/Register.tsx
@@ -1,2 +1,14 @@
-import React from "react";
+import * as React from 'react';
 import '../../styles/pages/SignUp/register.css'
+
+
+export interface IRegisterProps {
+}
+
+export function Register (props: IRegisterProps) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
